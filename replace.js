8ae2c57d8c6e24c9032f6fd50dbcf7d9bf60e203
Ed Flanagan <edsync256@gmail.com>
2014-11-22 00:58:53 -0500

    Update replace.js
    Instead of using the same replacement string all matches,
    generate a random combination for each match.

--- replace.js
+++ replace.js
@@ -1,11 +1,21 @@
-var dictS = ["Seriously", "Savagely", "Socially", "Strangely", "Spooky", "Sweet", "Sweaty", "Stupidly"];
-var dictJ = ["Judicious", "Jet-fueled", "Jurassic", "Juvenile", "Jumbo", "Jacked"];
-var dictW = ["Warthog", "Winnebago", "Wombat", "Weirdo", "Walrus", "Wookie"];
+var dictS = [
+    "Seriously", "Savagely", "Socially", "Strangely", "Spooky", "Sweet",
+    "Sweaty", "Stupidly"
+];
 
-randS = Math.floor(Math.random() * dictS.length);
-randJ = Math.floor(Math.random() * dictJ.length);
-randW = Math.floor(Math.random() * dictW.length);
+var dictJ = [
+    "Judicious", "Jet-fueled", "Jurassic", "Juvenile", "Jumbo", "Jacked"
+];
 
-var string = dictS[randS].concat(" ", dictJ[randJ], " ", dictW[randW]);
+var dictW = [
+    "Warthog", "Winnebago", "Wombat", "Weirdo", "Walrus", "Wookie"
+];
 
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("SJW", "g"), string);
+document.body.innerHTML = document.body.innerHTML.replace(/SJW/gi,
+    function() {
+            var randS = Math.floor(Math.random() * dictS.length);
+            var randJ = Math.floor(Math.random() * dictJ.length);
+            var randW = Math.floor(Math.random() * dictW.length);
+            return dictS[randS] + " " + dictJ[randJ] + " " + dictW[randW];
+    }
+);
