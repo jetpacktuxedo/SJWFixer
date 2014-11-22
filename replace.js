90308745582c98e8f7e6e6dfe6e6ebd7f949e3b1
jetpacktuxedo <crazeh.monkeh@gmail.com>
2014-11-22 01:36:18 -0500

    Fixed the netflix bug hopefully, thanks for open sourcing your code cloud-to-butt guys!

--- replace.js
+++ replace.js
@@ -2,12 +2,43 @@ var dictS = ["Seriously", "Savagely", "Socially", "Strangely", "Spooky", "Sweet"
 var dictJ = ["Judicious", "Jet-fueled", "Jurassic", "Juvenile", "Jumbo", "Jacked"];
 var dictW = ["Warthog", "Winnebago", "Wombat", "Weirdo", "Walrus", "Wookie"];
 
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("SJW", "g"),
-    function() {
-        randS = Math.floor(Math.random() * dictS.length);
-        randJ = Math.floor(Math.random() * dictJ.length);
-        randW = Math.floor(Math.random() * dictW.length);
+function walk(node){
+  // I stole this function from here:
+  // https://github.com/panicsteve/cloud-to-butt/blob/master/Source/content_script.js
+  // They stole it from here:
+  // http://is.gd/mwZp7E
+  var child, next;
+  switch ( node.nodeType )
+  {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
 
-        return dictS[randS].concat(" ", dictJ[randJ], " ", dictW[randW]);
-    }
-);
+function handleText(textNode){
+  var node = textNode.nodeValue;
+  node = node.replace(new RegExp("SJW", "g"), generate());
+  textNode.nodeValue = node;
+}
+
+function generate(){
+      randS = Math.floor(Math.random() * dictS.length);
+      randJ = Math.floor(Math.random() * dictJ.length);
+      randW = Math.floor(Math.random() * dictW.length);
+
+      return dictS[randS].concat(" ", dictJ[randJ], " ", dictW[randW]);
+}
+
+walk(document.body);
