ba692371fdc115831a5a0a602a3fe69cf27416cb
Ed Flanagan <edsync256@gmail.com>
2014-11-22 01:27:55 -0500

    "Improve" replacement
    Make the replacement more "robust" but using a modified version
    of https://github.com/panicsteve/cloud-to-butt

--- replace.js
+++ replace.js
@@ -11,11 +11,51 @@ var dictW = [
     "Warthog", "Winnebago", "Wombat", "Weirdo", "Walrus", "Wookie"
 ];
 
-document.body.innerHTML = document.body.innerHTML.replace(/SJW/gi,
-    function() {
-            var randS = Math.floor(Math.random() * dictS.length);
-            var randJ = Math.floor(Math.random() * dictJ.length);
-            var randW = Math.floor(Math.random() * dictW.length);
-            return dictS[randS] + " " + dictJ[randJ] + " " + dictW[randW];
+function random_sjw() {
+    var randS = Math.floor(Math.random() * dictS.length);
+    var randJ = Math.floor(Math.random() * dictJ.length);
+    var randW = Math.floor(Math.random() * dictW.length);
+
+    return dictS[randS] + " " + dictJ[randJ] + " " + dictW[randW];
+}
+
+function handleText(textNode) 
+{
+    var v = textNode.nodeValue;
+
+    v = v.replace(/\bSJW\b/g, random_sjw())
+
+    textNode.nodeValue = v;
+}
+
+function walk(node) 
+{
+    /*
+     * I stole this function from here:
+     *  (Cloud-to-Butt)
+     *  https://github.com/panicsteve/cloud-to-butt
+     * Who stole it from here:
+     *  http://is.gd/mwZp7E
+     */
+
+    var child, next;
+
+    switch (node.nodeType) {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
     }
-);
+}
+
+walk(document.body);
